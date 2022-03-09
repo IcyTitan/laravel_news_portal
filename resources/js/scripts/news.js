7cec52e2d5eb830b6e6e24b171a8963473fbd7ea
Максим Гаркунов <gr.kun.ov@gmail.com>
2022-03-10 03:04:48 +0700

    - К прошлому all

--- resources/js/scripts/news.js
+++ resources/js/scripts/news.js
@@ -1,23 +1,4 @@
 document.addEventListener("DOMContentLoaded", () => {
-    $( "#select-category" ).change(()=>{
-        let selectedCategory = $('#select-category').val();
-        $.ajax({
-            type: "POST",
-            dataType: 'JSON',
-            url: SET_CATEGORY,
-            headers: {
-                'X-CSRF-TOKEN': CSFR_TOKEN
-            },
-            data: {
-                category: selectedCategory,
-            },
-            success: function (response) {
-                console.log('response');
-                window.location.reload();
-            }
-        });
-    });
-
     $( "#select-pagination" ).change(()=>{
         let selectedCount = $('#select-pagination').val();
         $.ajax({
